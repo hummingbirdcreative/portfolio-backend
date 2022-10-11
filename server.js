d416586f6162f4951ec9a79f29e482bae6eee9a8
hummingbirdcreative <hellohummingbirdcreative@gmail.com>
2022-10-10 18:14:36 -1000

    added model, index route and create route in server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // Dependencies
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
+const morgan = require("morgan");
 
 // Initialize App
 const app = express();
+require("dotenv").config();
 
 // Configure Settings
 require('dotenv').config();
@@ -11,16 +14,49 @@ const { PORT = 4000, DATABASE_URL } = process.env;
 
 // Connect to MongoDB using Mongoose
 mongoose.connect(DATABASE_URL);
+app.use(cors()); 
+app.use(morgan("dev")); 
+app.use(express.json()); 
 
 // Mount Middleware
 mongoose.connection
 .on('connected', () => console.log('Connected to MongoDB'))
 .on('error', (error) => console.log(`MongoDB Error: ${error.message}`));
 
+// Define models
+const Schema = mongoose.Schema;
+const projectsSchema = new Schema({
+    image: String,
+    title: String,
+    description: String,
+    link: String,
+}, { timestamps: true });
+
+const Projects = mongoose.model('Projects', projectsSchema)
+
 // Mount Routes
 app.get('/', (req, res) => {
   res.send('hello portfolio');
 });
+
+// Routes/Controllers
+// Index
+app.get('/projects', async (req, res) => {
+    try {
+      res.status(200).json(await Projects.find({}));
+    } catch (error) {
+      res.status(400).json({ message: 'bad request' });
+    }
+  });
+
+// Create
+app.post('/projects', async (req, res) => {
+    try {
+      res.status(201).json(await Projects.create(req.body));
+    } catch (error) {
+      res.status(400).json({ message: 'bad request' });
+    }
+  });
 
 // Tell app to listen
 app.listen(PORT, () => {
